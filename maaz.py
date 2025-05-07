7a491abe160ed14a4a75ea8cf4da2ed928fe87fe
Maazihsan <maazihsan302@gmail.com>
2025-05-07 22:37:02 +0500

    initial commit

---
+++ maaz.py
@@ -0,0 +1,9 @@
+light=input("enter the light color:")
+if ("light==green"):
+    print("go")
+elif ("light==yellow"):
+    print("wait")
+elif ("light==red"):
+    print("stop")
+else:
+    print("invalid light")
