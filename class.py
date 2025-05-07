65a400fe36f4290e15e58dbe0703ca28d882b6bb
Maazihsan <maazihsan302@gmail.com>
2025-05-07 23:27:30 +0500

    initial commit

---
+++ class.py
@@ -0,0 +1,9 @@
+class student:
+    def __init__(self,name,age,rollno):
+        self.name =name
+        self.age =age 
+        self.rollno=rollno
+
+p1= student("maaz",21,620)
+print(p1.name , p1.age , p1. rollno)
+    
--- maaz.py
+++
@@ -1,9 +0,0 @@
-light=input("enter the light color:")
-if ("light==green"):
-    print("go")
-elif ("light==yellow"):
-    print("wait")
-elif ("light==red"):
-    print("stop")
-else:
-    print("invalid light")
